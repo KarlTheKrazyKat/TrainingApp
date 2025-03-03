ec4641eafafe5b1e5a5c3d123b2a1e970b67b57e
KarlTheKrazyKat <elijah2005l@gmail.com>
2025-03-03 10:51:15 -0500

    project creation is starting to come along

--- Screens/Config/f_controls.py
+++ Screens/Config/f_controls.py
@@ -3,9 +3,11 @@ from tkinter import *
 from tkinter import ttk
 from Screens.root import *
 #File Specific Imports
+from json import *
+from modules.Config.m_selector import _m_selector, _m_muscles
 
 #Create Frame
-f_controls = ttk.Frame(root,height=60,width=200,relief='sunken',padding=50)
+f_controls = ttk.Frame(root,height=150,width=200,relief='sunken',padding=25)
 f_controls.grid(column=0,row=0,columnspan=1,rowspan=1,sticky=(N, S, E, W))
 
 #Configure Frame Grid
@@ -22,6 +24,13 @@ def findAll():
     print("Will search in all plans and load all muscles")
 
 def newMusc():
+    with open("../TrainingApp/Screens/Config/Menus/selected.json","r") as f:
+        md = load(f)
+    md[muscname.get()]=False
+    with open("../TrainingApp/Screens/Config/Menus/selected.json","w") as f:
+                dump(md, f, indent =2)
+    _m_selector()
+    _m_muscles()
     print("Will create a new musc element called ",muscname.get())
 
 #Navigation
---
+++ modules/Config/m_controls.py
@@ -0,0 +1,5 @@
+from modules.Config.m_selector import *
+from json import *
+from Screens.Config.f_controls import *
+
+#buttons do everything?
